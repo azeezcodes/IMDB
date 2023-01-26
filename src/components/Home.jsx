11295f084ffe0e6d955f286709bfa4563073bc90
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2023-01-26 15:10:18 +0100

    pagination done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import ReactPaginate from "react-paginate";
 
 const Home = () => {
 
@@ -23,12 +24,27 @@ const Home = () => {
      loaddata()
    }, [])
 
- 
+ //react-paginate
+   const [itemOffset, setItemOffset] = useState(0);
+    const endOffset = itemOffset + 12;
+    const currentItems = movies.slice(itemOffset, endOffset);
+    const pageCount = Math.ceil(movies.length / 12);
+
+
+    const handlePageClick = (event) => {
+       const newOffset = (event.selected * 12) % movies.length;
+       setItemOffset(newOffset);
+    };
+   
+   
 
     return (
-       <div className="bg-[#0D0D0F] grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-3 justify-items-center">
-          {movies.map((movie) => (
-             <div key={movie.id} className="h-[398px] w-[332px] bg-[#161616] flex flex-col justify-center items-center">
+       <div className="bg-[#0D0D0F] grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-3 justify-items-center mt-[60px]">
+          {currentItems.map((movie) => (
+             <div
+                key={movie.id}
+                className="h-[398px] w-[332px] bg-[#161616] flex flex-col justify-center items-center"
+             >
                 <Link to={`/movie/${movie.id}`}>
                    <img
                       src={movie.image}
@@ -39,6 +55,26 @@ const Home = () => {
                 <p className="font-bold text-white py-3">{movie.title}</p>
              </div>
           ))}
+
+          <div className="mt-10 ml-[600px] flex items-center justify-center ">
+             <ReactPaginate
+                breakLabel="..."
+                nextLabel="Next"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={3}
+                pageCount={pageCount}
+                breakClassName={"text-white ml-[20px] mr-[20px]"}
+                previousLabel="Previous"
+                containerClassName={"flex items-center w-[400px]"}
+                previousClassName={
+                   "bg-[#5FCF7B] text-black rounded-md px-3 py-1 h-10 hover:bg-[#85db9a]"
+                }
+                nextClassName={
+                   "bg-[#5FCF7B] text-black rounded-md px-3 py-1 h-10 hover:bg-[#85db9a]"
+                }
+                pageClassName={"text-white ml-[20px] mr-[20px]"}
+             />
+          </div>
        </div>
     );
 }
